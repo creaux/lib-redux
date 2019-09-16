21ebae4c5f70d3650e49b9dece810319ef5c2e5e
Petr Juna <petr.juna@gmail.com>
2019-09-16 22:34:37 +0200

    fix(lib-model): test fix

--- src/test.ts
+++ src/test.ts
@@ -1,25 +1,27 @@
 import test from 'ava';
 import { configureStore } from './store';
 import { requestPostsAction, createPostAction } from './post/actions';
-import { PostStateEnum } from '@pyxismedia/lib-model/post';
+import { PostStateEnum } from '@pyxismedia/lib-model';
 
 const store = configureStore();
 
-test.before(t => {
-    store.dispatch(createPostAction({
-        title: 'Test 1',
-        subtitle: 'Subtitle Test 1',
-        content: 'Lorem ispum',
-        image: '#',
-        state: PostStateEnum.DRAFT,
-        labels: ['label 1'],
-        createdBy: 'Franta',
-        section: 'One'
-    }))
+test.before(() => {
+  store.dispatch(
+    createPostAction({
+      title: 'Test 1',
+      subtitle: 'Subtitle Test 1',
+      content: 'Lorem ispum',
+      image: '#',
+      state: PostStateEnum.DRAFT,
+      labels: ['label 1'],
+      createdBy: 'Franta',
+      section: 'One',
+    }),
+  );
 });
 
 test('should deliver posts', t => {
-    store.dispatch(requestPostsAction());
-    console.log(store.getState());
-    t.deepEqual(store.getState(), { post: {} });
+  store.dispatch(requestPostsAction());
+  console.log(store.getState());
+  t.deepEqual(store.getState(), { post: {} });
 });
