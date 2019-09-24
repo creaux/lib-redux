bb2befa4c5f8db31f684ca204825a29fda5926fd
Petr Juna <petr.juna@gmail.com>
2019-09-24 13:56:07 +0200

    fix(build): eslint clean

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 import babel from 'rollup-plugin-babel';
-import { eslint } from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default {
   input: './src/index.ts',
@@ -8,10 +11,11 @@ export default {
     format: 'cjs',
   },
   plugins: [
+    resolve({ extensions }),
+    commonjs(),
     babel({
-      exclude: 'node_modules/**',
-      extensions: ['.ts', '.js'],
+      include: 'src/**/*',
+      extensions,
     }),
-    eslint(),
   ],
 };
