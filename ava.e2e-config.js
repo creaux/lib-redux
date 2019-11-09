81e1cd73aa8427f5ce1294067149db9e384b1de1
Petr Juna <petr.juna@gmail.com>
2019-11-09 14:27:39 +0100

    chore(e2e): fix

--- ava.e2e-config.js
+++ ava.e2e-config.js
@@ -1,7 +1,8 @@
 export default {
-  files: ['./e2e/**/*.e2e-spec.ts'],
+  files: ['e2e/**/*.e2e-spec.ts'],
   compileEnhancements: false,
   require: ['ts-node/register', './src/env.ts'],
+  extensions: ['ts'],
   // https://github.com/avajs/ava/issues/2291
   sources: ['src/**/*', 'node_modules/@pyxismedia/**/*'],
 };
